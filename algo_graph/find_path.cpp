b71f9cce65db9e0b96831f9f10ad1c5842ef25a6
Soppphie <if23b290@technikum-wien.at>
2024-06-04 12:37:53 +0200

    Zwischenpush, nur Änderungen im Main

--- algo_graph/find_path.cpp
+++ algo_graph/find_path.cpp
@@ -1,20 +1,51 @@
-// algo_graph.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+//Werte je Station
+struct Station
+{
+    std::string station_name;
+    int travel_duration;
+};
+
+class Linie
+{
+public:
+    std::string line;
+    //Liste der Stationen:
+    std::vector<Station> stations;
+
+    //Stationen zur Linie hinzufügen:
+    void add_station(const std::string& station_name, int travel_duration) 
+    {
+        stations.push_back({ station_name, travel_duration });
+    }
+};
+
+void read_file(const std::string& filename, std::vector<Linie>& lines)
+{
+    std::ifstream infile(filename);
+    if (!infile)
+    {
+        std::cerr << "Fehler beim oeffnen der Datei!" << std::endl;
+        return;
+    }
+    
+   
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //bereits markierte Knoten müssen vermerkt werden, mögl. bool
+    bool prev_visited = false;
+
+    //????
+    std::vector<Linie> lines;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
