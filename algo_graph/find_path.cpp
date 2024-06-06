f577bab9f402f17c0b1bda9753da90a4731095b7
Soppphie <if23b290@technikum-wien.at>
2024-06-06 17:04:49 +0200

    File einlesen fertig

--- algo_graph/find_path.cpp
+++ algo_graph/find_path.cpp
@@ -38,20 +38,26 @@ void read_file(const std::string& filename, std::vector<Linie>& lines)
     {
         std::istringstream iss(line);
         std::string line_name;
+        std::string station_name;
+        int travel_duration;
 
         if (std::getline(iss, line_name, ':'))
         {
             Linie new_line;
             new_line.line = line_name;
 
-            std::string station_name;
-            int travel_duration;
 
             while(iss >> std::quoted(station_name) >> travel_duration) //quoted für ""
             {
                 new_line.add_station(station_name, travel_duration);
             }
+            //Letzte Station einlesen:
+            if (iss.eof())
+            {
+                new_line.add_station(station_name, travel_duration);
+            }
             lines.push_back(new_line);
+  
         }
     }
     infile.close();
